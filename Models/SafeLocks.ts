6ec37302001b53b331f044c8d2ac274fa8856757
nicsylvia <nicsylvia15f@gmail.com>
2023-03-01 11:04:05 -0800

    saved safelocks models

--- Models/SafeLocks.ts
+++ Models/SafeLocks.ts
@@ -5,5 +5,26 @@ import { LocksData } from "../AllInterfaces/AllInterface"
 interface MainLocksData extends LocksData, Document{}
 
 const SafeLocksSchema = new Schema<LocksData>({
-    
-})
+    amount: {
+        type: Number,
+    },
+    lock: {
+        type: Boolean
+    },
+    PayBackTime: {
+        type: String
+    },
+    interest: {
+        type: Number
+    },
+    title: {
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+const SafeLocksModels = model<MainLocksData>("SafeLocks", SafeLocksSchema);
+
+export default SafeLocksModels
